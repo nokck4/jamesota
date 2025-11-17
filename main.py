28f6ecb115a37b3159cde751138a972d139fd8fa
nokck4 <phoonweeraphan@gmail.com>
2025-11-17 15:28:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+from machine import Pin
+from time import sleep
+
+# กำหนดขา LED บนบอร์ด (Pico/Pico W ใช้ GPIO25)
+led = Pin("LED", Pin.OUT)
+
+while True:
+    led.value(1)  # เปิด LED
+    sleep(0.5)    # รอ 0.5 วินาที
+    led.value(0)  # ปิด LED
+    sleep(0.5)    # รอ 0.5 วินาที
+    print("hello world")
+
+
+
